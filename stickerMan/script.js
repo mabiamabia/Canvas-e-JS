37949b103d2216ed8bd9b77fde5e397c9ff3fde0
priscila.costa <priscila.costa@g.globo>
2022-09-14 18:06:36 -0300

    create head for the stickman

---
+++ stickerMan/script.js
@@ -0,0 +1,18 @@
+let canvas = document.getElementById("stickMan");
+let context = canvas.getContext("2d");
+
+let cor = "#000";
+
+canvas.width = 400;
+canvas.heigth = 300;
+
+context.fillStyle = cor; //preenchimento
+context.strokeStyle = cor; //cor da linha
+
+context.lineWidth = 5;
+
+context.beginPath();
+
+context.arc(200, 50, 20, 0, Math.PI * 2);
+
+context.fill();
